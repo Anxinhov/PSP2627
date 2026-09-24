17722a3bf5d8b8b60e1eef2ac6d0d5081e449c94
anxinhov <anxo.vazquez.manzano@iessanmamede.com>
2026-09-24 10:05:11 +0200

    UD01_T01 psutil ejercicio4 inicio

---
+++ ejercicios/ud01_t01_psutil/scripts/ejercicio1/ejercicio4.py
@@ -0,0 +1,13 @@
+#Información de discos
+import psutil
+#Listado de particiones
+print(psutil.disk_partitions(all=False))
+#Uso de disco para cada unidad o partición
+
+#Número de operaciones de lectura
+
+#Número de operaciones de escritura
+
+#Número de bytes leídos
+
+#Número de bytes escritos
