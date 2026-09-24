902a7d9abb3d8e6a123f9fe63b2f7a3dfc81f7d8
anxinhov <anxo.vazquez.manzano@iessanmamede.com>
2026-09-24 09:54:56 +0200

    UD01_T01 psutil ejercicio3 final

--- ejercicios/ud01_t01_psutil/scripts/ejercicio1/ejercicio3.py
+++ ejercicios/ud01_t01_psutil/scripts/ejercicio1/ejercicio3.py
@@ -1,7 +1,8 @@
 #Información de memoria
 import psutil
 #Memoria total
-
+print(psutil.virtual_memory().total)
 #Memoria disponible
-
+print(psutil.virtual_memory().available)
 #Porcentaje de memoria usada
+print(psutil.virtual_memory().percent)
